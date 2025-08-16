5ba2858a8a8e3ae995ffbd83bb8cff54092f1755
Hamid Yaftian <hamid.yaftian@gmail.com>
2025-08-16 16:19:16 +0330

    fix: auto import css file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { join, resolve } from 'node:path';
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import dts from 'vite-plugin-dts';
 
 import { peerDependencies } from './package.json';
@@ -25,6 +26,7 @@ export default defineConfig({
     },
     plugins: [
         react(),
+        cssInjectedByJsPlugin(),
         dts({ rollupTypes: true }), // Output .d.ts files
     ],
     test: {
