8e6a200c7d1dd3d9e3b0107df2faa688cf954c2c
Hamid Yaftian <hamid.yaftian@gmail.com>
2025-08-17 17:46:41 +0330

    chore: remove unused vite-plugin-css-injected-by-js from package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { join, resolve } from 'node:path';
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-// import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import dts from 'vite-plugin-dts';
 
 import { peerDependencies } from './package.json';
@@ -30,7 +29,6 @@ export default defineConfig({
     },
     plugins: [
         react(),
-        // cssInjectedByJsPlugin(),
         dts({ rollupTypes: true }),
     ],
     test: {
