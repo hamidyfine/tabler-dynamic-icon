65bf1d733d708a04b70901c409efa9b1b001c89a
Hamid Yaftian <hamid.yaftian@gmail.com>
2025-11-07 12:07:16 +0330

    refactor: make icons property optional in IconsContext interface

--- src/icon/icon.provider.tsx
+++ src/icon/icon.provider.tsx
@@ -2,17 +2,13 @@ import type { PropsWithChildren } from 'react';
 import { createContext } from 'react';
 
 export interface IIconsContext {
-    icons: Record<string, any>;
+    icons?: Record<string, any>;
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const IconsContext = createContext<IIconsContext | undefined>(undefined);
 
-interface IIconsProviderProps extends PropsWithChildren {
-    icons: Record<string, any>;
-}
-
-export const IconsProvider = ({ children, icons }: IIconsProviderProps) => {
+export const IconsProvider = ({ children, icons }: PropsWithChildren<IIconsContext>) => {
     return (
         <IconsContext.Provider value={{ icons }}>
             {children}
